6f8bb720bea2ef88a444c6f54cfec08342e38d89
Jordan <jordan.m.singer@gmail.com>
2018-05-19 13:41:59 -0500

    Changing parameter template file to accept momentum.

--- src/network.py
+++ src/network.py
@@ -88,7 +88,7 @@ class network:
 
             if self.parameters['training']['optimizer'] == 'adam':
                 self.optimizer = tf.train.AdamOptimizer(self.learning_rate[0])
-            elif self.parameters['training']['otpimizer'] == 'momentum':
+            elif self.parameters['training']['optimizer'] == 'momentum':
                 self.optimizer = tf.train.MomentumOptimizer(self.learning_rate[0], momentum=self.parameters['training']['optimizer']['momentum'])
             else:  # Default to SGD
                 self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate[0])
