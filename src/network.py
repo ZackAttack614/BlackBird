9bffa14f8fec451dd1911c3eea7607ff43c7a229
Jordan <jordan.m.singer@gmail.com>
2018-05-19 21:26:29 -0500

    Cleaning up evaluation

--- src/network.py
+++ src/network.py
@@ -38,9 +38,9 @@ class network:
         """ Build out the policy/evaluation combo network
         """
         with tf.variable_scope('inputs', reuse=tf.AUTO_REUSE) as scope:
-            self.input = tf.placeholder(shape=[1, self.dims[0], self.dims[1], 3], name='board_input', dtype=tf.float32)
+            self.input = tf.placeholder(shape=[None, self.dims[0], self.dims[1], 3], name='board_input', dtype=tf.float32)
             self.correct_move_vec = tf.placeholder(shape=[None], name='correct_move_from_mcts', dtype=tf.float32)
-            self.mcts_evaluation = tf.placeholder(shape=[1], name='mcts_evaluation', dtype=tf.float32)
+            self.mcts_evaluation = tf.placeholder(shape=[None], name='mcts_evaluation', dtype=tf.float32)
             
         with tf.variable_scope('hidden', reuse=tf.AUTO_REUSE) as scope:
             self.hidden = [self.input]
@@ -58,9 +58,8 @@ class network:
             self.eval_batch_norm = tf.layers.batch_normalization(self.eval_conv, name='batch_norm')
             self.eval_rect_norm = tf.nn.relu(self.eval_batch_norm, name='rect_norm')
             self.eval_dense = tf.layers.dense(inputs=self.eval_rect_norm, units=self.parameters['eval']['dense'], name='dense', activation=tf.nn.relu)
-            self.eval_slice = tf.slice(input_=self.eval_dense, begin=[0,0,0,0], size=[1,1,1,self.parameters['eval']['dense']])
-            self.eval_scalar = tf.layers.dense(inputs=self.eval_slice, units=1, name='scalar')
-            self.evaluation = tf.tanh(self.eval_scalar, name='evaluation')[0][0][0][0]
+            self.eval_scalar = tf.reduce_sum(self.eval_dense, axis=[1,2,3])
+            self.evaluation = tf.tanh(self.eval_scalar, name='evaluation')
             
         with tf.variable_scope('policy', reuse=tf.AUTO_REUSE) as scope:
             self.epsilon = tf.placeholder(shape=[1], dtype=tf.float32)
@@ -102,7 +101,7 @@ class network:
         """ Given a game state, return the network's evaluation.
         """
         evaluation = self.sess.run(self.evaluation, feed_dict={self.input:state})
-        return evaluation
+        return evaluation[0]
     
     def getPolicy(self, state):
         """ Given a game state, return the network's policy.
