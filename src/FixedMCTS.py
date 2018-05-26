4b41d8aa1238da182d83b50d5f88e6ce8b048f40
Jordan <jordan.m.singer@gmail.com>
2018-05-26 14:51:54 -0500

    Restructure init, fix assert

--- src/FixedMCTS.py
+++ src/FixedMCTS.py
@@ -2,10 +2,20 @@ from mcts import MCTS,Node
 import numpy as np
 
 class FixedMCTS(MCTS):
-    """An implementation of Monte Carlo Tree Search that only aggregates statistics up to a fixed depth."""
-    def __init__(self, maxDepth, explorationRate, threads = 1, timeLimit = None, playLimit = None, **kwargs):
-        self.MaxDepth = maxDepth
-        return super().__init__(explorationRate, timeLimit, playLimit, threads, **kwargs)
+    """ An implementation of Monte Carlo Tree Search that only aggregates 
+        statistics up to a fixed depth.
+    """
+    def __init__(self, parameters):
+        self.parameters = parameters['mcts']
+        self.MaxDepth = self.parameters['maxDepth']
+        explorationRate = self.parameters['explorationRate']
+        timeLimit = self.parameters['timeLimit']
+        playLimit = self.parameters['playLimit']
+        threads = self.parameters['threads']
+
+        assert self.MaxDepth > 0, 'MaxDepth for MCTS must be > 0.'
+
+        super().__init__(explorationRate, timeLimit, playLimit, threads)
     
     # Overriding from MCTS
     def FindLeaf(self, node):
@@ -19,7 +29,7 @@ class FixedMCTS(MCTS):
                 break
             lastAction = self._selectAction(node)
             node = node.Children[lastAction]
-        assert i > 0, 'When requesting a move from the MCTS, there is at least one legal option.'
+            
         return node
 
 
