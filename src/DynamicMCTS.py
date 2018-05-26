12885be5d0d02a283dc75cc2532ffd4b67283270
Jordan <jordan.m.singer@gmail.com>
2018-05-26 14:49:18 -0500

    Modify lines to fit in 80 chars

--- src/DynamicMCTS.py
+++ src/DynamicMCTS.py
@@ -2,14 +2,19 @@ from mcts import MCTS,Node
 import numpy as np
 
 class DynamicMCTS(MCTS):
-    """An implementation of Monte Carlo Tree Search that aggregates statistics as it explores the tree"""
-    def __init__(self, explorationRate, threads = 1, timeLimit = None, playLimit = None, **kwargs):
-        return super().__init__(explorationRate, timeLimit, playLimit, threads, **kwargs)
+    """ An implementation of Monte Carlo Tree Search that aggregates statistics
+        as it explores the tree
+    """
+    def __init__(self, explorationRate, threads = 1, timeLimit = None,
+        playLimit = None, **kwargs):
+        return super().__init__(explorationRate, timeLimit, playLimit, threads,
+            **kwargs)
     
     # Overriding from MCTS
     def FindLeaf(self, node):
         while True:
-            # If we are at the edge of our currently explored branch, build out the children and stop.
+            # If we are at the edge of our currently explored branch, 
+            # build out the children and stop.
             if node.Children is None:
                 self.AddChildren(node)
                 break
